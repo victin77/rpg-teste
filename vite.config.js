65e2a918b5a0ee4c83eb5c0645b99241f4f754f3
Victor <142611471+victin77@users.noreply.github.com>
2026-01-08 21:47:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'rpg-teste-production.up.railway.app'
+    ]
   }
-});
+})
